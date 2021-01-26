197c83eb8fb82e279926c1a9b6aa15841f585b49
TheAlexSannikov <sannikov@wisc.edu>
2021-01-26 12:09:19 -0600

    added front CW rotation. Bug persists.

--- src/Cube.js
+++ src/Cube.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Grid } from '@material-ui/core';
 import CubeRow from './CubeRow';
+import { lightGreen } from '@material-ui/core/colors';
 
 const colors = ['WHITE', 'BLUE', 'RED', 'GREEN', 'ORANGE', 'YELLOW']; // dictates the starting position; and center pieces 
 const faceNames = ['BOTTOM', 'FRONT', 'RIGHT', 'BACK', 'LEFT', 'TOP'];
@@ -17,6 +18,7 @@ class Cube extends React.Component {
           this.lookToLeftFace = this.lookToLeftFace.bind(this);
           this.lookToTopFace = this.lookToTopFace.bind(this);
           this.lookToBottomFace = this.lookToBottomFace.bind(this);
+          this.rotateFrontCW = this.rotateFrontCW.bind(this);
      }
 
      rotateFace(face, dir) { //lookToRight: top -> cw; bottom -> ccw
@@ -77,7 +79,7 @@ class Cube extends React.Component {
           newFaces["BACK"] = faces["LEFT"];
           newFaces["LEFT"] = faces["FRONT"];
 
-          newFaces["TOP"] = this.rotateFace(faces["TOP"], 'cw'); 
+          newFaces["TOP"] = this.rotateFace(faces["TOP"], 'cw');
           newFaces["BOTTOM"] = this.rotateFace(faces["BOTTOM"], 'ccw');
           await this.setState({ faces: newFaces, });
           console.log(this.state.faces);
@@ -90,7 +92,7 @@ class Cube extends React.Component {
           newFaces["LEFT"] = faces["BACK"];
           newFaces["BACK"] = faces["RIGHT"];
           newFaces["RIGHT"] = faces["FRONT"];
-          newFaces["TOP"] = this.rotateFace(faces["TOP"], 'ccw'); 
+          newFaces["TOP"] = this.rotateFace(faces["TOP"], 'ccw');
           newFaces["BOTTOM"] = this.rotateFace(faces["BOTTOM"], 'cw');
           await this.setState({ faces: newFaces, });
           console.log(this.state.faces);
@@ -105,7 +107,7 @@ class Cube extends React.Component {
           newFaces["BOTTOM"] = faces["FRONT"];
 
           newFaces["LEFT"] = this.rotateFace(faces["LEFT"], "cw");
-          newFaces["RIGHT"] = this.rotateFace(faces["RIGHT"],"ccw")
+          newFaces["RIGHT"] = this.rotateFace(faces["RIGHT"], "ccw")
           await this.setState({ faces: newFaces, });
           console.log(this.state.faces);
      }
@@ -118,9 +120,55 @@ class Cube extends React.Component {
           newFaces["BACK"] = faces["TOP"];
           newFaces["TOP"] = faces["FRONT"];
           newFaces["LEFT"] = this.rotateFace(faces["LEFT"], "ccw");
-          newFaces["RIGHT"] = this.rotateFace(faces["RIGHT"],"cw")
+          newFaces["RIGHT"] = this.rotateFace(faces["RIGHT"], "cw")
           await this.setState({ faces: newFaces, });
           console.log(this.state.faces);
+     }
+
+     rotateFrontCW(cube) {
+          if (cube == undefined || cube["FRONT"] === undefined)
+               return
+
+          let newCube = {};
+          // for (const faceName in cube) { //copy cube
+          //      const face = object[faceName];
+          //      newCube[faceName] = face;
+          // }
+
+
+          let leftFace = JSON.parse(JSON.stringify(cube["LEFT"]));
+          let topFace = JSON.parse(JSON.stringify(cube["TOP"]));
+          let rightFace = JSON.parse(JSON.stringify(cube["RIGHT"]));
+          let bottomFace = JSON.parse(JSON.stringify(cube["BOTTOM"]));
+          console.log(leftFace)
+
+          //lazy algorithm
+          leftFace[0][2] = cube["BOTTOM"][0][0];
+          leftFace[1][2] = cube["BOTTOM"][0][1];
+          leftFace[2][2] = cube["BOTTOM"][0][2];
+
+          console.log(cube);
+
+          topFace[2][0] = cube["LEFT"][0][2];
+          topFace[2][1] = cube["LEFT"][1][2];
+          topFace[2][2] = cube["LEFT"][2][2];
+
+          rightFace[0][0] = cube["TOP"][2][0];
+          rightFace[1][0] = cube["TOP"][2][1];
+          rightFace[2][0] = cube["TOP"][2][2];
+
+          bottomFace[0][0] = cube["RIGHT"][0][0];
+          bottomFace[0][1] = cube["RIGHT"][1][0];
+          bottomFace[0][2] = cube["RIGHT"][2][0];
+
+          newCube["FRONT"] = this.rotateFace(cube["FRONT"], "cw");
+          newCube["BACK"] = cube["BACK"];
+          newCube["LEFT"] = leftFace;
+          newCube["TOP"] = topFace;
+          newCube["RIGHT"] = rightFace;
+          newCube["BOTTOM"] = bottomFace;
+          console.log(newCube)
+          return newCube;
      }
 
      getDisplayedFace() {
@@ -170,15 +218,25 @@ class Cube extends React.Component {
                     <Grid container spacing={1}>
 
                          <Grid className={'top'} container item xs={12} spacing={3}>
-                              <CubeRow row={top} rowNumber={0}/>
+                              <CubeRow row={top} rowNumber={0} />
                          </Grid>
                          <Grid className={'meat'} container item xs={12} spacing={3}>
-                              <CubeRow row={meat} rowNumber={1}/>
+                              <CubeRow row={meat} rowNumber={1} />
                          </Grid>
                          <Grid className={'bottom'} container item xs={12} spacing={3}>
-                              <CubeRow row={bottom} rowNumber={2}/>
+                              <CubeRow row={bottom} rowNumber={2} />
                          </Grid>
                     </Grid>
+                    <div className="controls controls-bottom">
+                         <label> rotate front CW
+                         <input
+                                   type="checkbox"
+                                   className="checkbox"
+                                   // value="1"
+                                   // checked={}
+                                   onChange={() => { this.setState({ faces: this.rotateFrontCW(this.state.faces) }); }} />
+                         </label>
+                    </div>
                </>
           );
      }
